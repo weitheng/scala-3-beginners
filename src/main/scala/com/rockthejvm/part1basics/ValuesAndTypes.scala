7e5b5a711c4b1b189894b6ef5c7f2ee5f7708cd3
wthenghaw <wei.thenghaw@disneystreaming.com>
2021-12-03 01:02:37 +0000

    Learn about Values and Types

--- src/main/scala/com/rockthejvm/part1basics/ValuesAndTypes.scala
+++ src/main/scala/com/rockthejvm/part1basics/ValuesAndTypes.scala
@@ -1,6 +1,26 @@
 package com.rockthejvm.part1basics
 
 object ValuesAndTypes {
+
+  // values
+  val meaningOfLife: Int = 42 // const int meaningofLife = 42
+
+  //assigning is not allowed
+  // meaningOfLife = 45
+
+  // type inference
+  val anInteger = 67 // : Int is optional, compiler can tell the type
+
+  // common types
+  val aBoolean : Boolean = true // or false
+  val aChar : Char = 'a' // single quote
+  val aString : String = "" // double quote
+  val anInt : Int = 78 // 4 bytes representation
+  val aShort : Short = 5263 // 2 bytes representation -2^16 -> + 2^16
+  val aLong : Long = 894172398427L // L at the end differentiate Long from Int, 8 bytes representation
+  val aFloat : Float = 2.4f // 4 bytes representation, with decimals
+  val aDouble : Double = 3.14 // 8 byes representation
+
   def main(args: Array[String]): Unit = {
 
   }
